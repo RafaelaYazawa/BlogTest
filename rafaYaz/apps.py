74be0d39912ebedecd7436dda096d2372ebedd8e
rafaela yazawa <yazawrafa@gmail.com>
2026-01-16 14:02:52 +0900

    Initialize app's models and admin

--- rafaYaz/apps.py
+++ rafaYaz/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class RafayazConfig(AppConfig):
+class RafaYazConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'rafaYaz'
